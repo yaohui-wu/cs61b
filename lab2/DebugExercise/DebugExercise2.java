cb266c0fb4001cbbceb4fd6610032d492bd9988f
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-08-06 22:53:23 -0400

    Update Lab 2: Debugging

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -39,7 +39,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum += x[i];
             i = i + 1;
         }
         return sum;
