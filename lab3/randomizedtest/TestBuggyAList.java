3155f0e3601551a320c376e6374480cdaeaa0ba4
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-13 17:23:08 -0500

    Update Lab 3: Timing Tests and Randomized Comparison Tests

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,20 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+
+  /** Adds the same value to both the correct and buggy AList implementations,
+   * then checks that the results of three subsequent removeLast calls are the same. */
+  @Test
+  public void TestThreeAddThreeRemove() {
+    AListNoResizing<Integer> correctList = new AListNoResizing<>();
+    BuggyAList<Integer> buggyList = new BuggyAList<>();
+    for (int i = 0; i < 3; i++) {
+      correctList.addLast(i);
+      buggyList.addLast(i);
+    }
+    assertEquals(correctList.size(), buggyList.size());
+    for (int i = 0; i < 3; i++) {
+      assertEquals(correctList.removeLast(), buggyList.removeLast());
+    }
+  }
 }
