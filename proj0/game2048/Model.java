8c495d15edd6a08fab25042ea40fa299a2da0877
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-13 14:00:17 -0500

    Update Project 0: 2048

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -110,9 +110,6 @@ public class Model extends Observable {
         boolean changed;
         changed = false;
 
-        // TODO: Modify this.board (and perhaps this.score) to account
-        // for the tilt to the Side SIDE. If the board changed, set the
-        // changed local variable to true.
         board.setViewingPerspective(side);
         int length = board.size();
         for (int col = 0; col < length; col++) {
