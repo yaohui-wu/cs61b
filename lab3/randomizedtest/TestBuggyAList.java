ffd1471f73dec3541d4ca09048f869bd0000139d
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-13 17:33:46 -0500

    Update Lab 3: Timing Tests and Randomized Comparison Tests

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -24,4 +24,24 @@ public class TestBuggyAList {
       assertEquals(correctList.removeLast(), buggyList.removeLast());
     }
   }
+
+  @Test
+  public void randomizedTest() {
+    AListNoResizing<Integer> L = new AListNoResizing<>();
+
+    int N = 500;
+    for (int i = 0; i < N; i += 1) {
+        int operationNumber = StdRandom.uniform(0, 2);
+        if (operationNumber == 0) {
+            // addLast
+            int randVal = StdRandom.uniform(0, 100);
+            L.addLast(randVal);
+            System.out.println("addLast(" + randVal + ")");
+        } else if (operationNumber == 1) {
+            // size
+            int size = L.size();
+            System.out.println("size: " + size);
+        }
+    }
+  }
 }
