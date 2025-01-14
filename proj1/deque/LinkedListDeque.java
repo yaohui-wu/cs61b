bc2faadf8036ae778187aba370659b73adb6f29b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-14 17:07:48 -0500

    Update Project 1: Data Structures

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,56 +1,42 @@
 package deque;
 
-/**
- * @author Yaohui Wu
+/** Implementation of a deque (double-ended queue) using a doubly linked list
+ *  with a sentinel node.
+ *  @author Yaohui Wu
  */
 public class LinkedListDeque<T> {
 
     public static class Node<T> {
         private T item;
         private Node<T> next;
+        private Node<T> previous;
 
         public Node(T item) {
             this.item = item;
             this.next = null;
-        }
-
-        public Node(T item, Node<T> next) {
-            this.item = item;
-            this.next = next;
+            this.previous = null;
         }
 
         public T getitem() { return item; }
     }
 
 
-    private Node<T> head;
-    private Node<T> tail;
+    private Node<T> sentinel;
     private int size;
 
     public LinkedListDeque() {
-        head = null;
-        tail = null;
+        sentinel = null;
         size = 0;
     }
 
-    public LinkedListDeque(T item) {
-        head = new Node<>(item);
-        tail = head;
-        size = 1;
-    }
-
     public void addFirst(T item) {
-        Node<T> first = new Node<>(item, head);
-        head = first;
+        Node<T> first = new Node<>(item);
+        first.next = sentinel;
+        sentinel = first;
         size++;
     }
 
-    public void addLast(T item) {
-        Node<T> last = new Node<>(item);
-        tail.next = last;
-        tail = last;
-        size++;
-    }
+    public void addLast(T item) {}
 
     public boolean isEmpty() { return size == 0; }
 
@@ -58,7 +44,7 @@ public class LinkedListDeque<T> {
     
     public void printDeque() {
         for (int i = 0; i < size; i++) {
-            Node<T> current = head;
+            Node<T> current = sentinel;
             System.out.print(current.item);
             current = current.next;
         }
@@ -67,14 +53,12 @@ public class LinkedListDeque<T> {
 
     public T removeFirst() {
         if (size <= 0) { return null; }
-        T firstItem = head.item;
-        head = head.next;
+        T firstItem = sentinel.item;
+        sentinel = sentinel.next;
         return firstItem;
     }
 
     public T removeLast() {
         if (size <= 0) { return null; }
-        T lastItem = tail.item;
-        return lastItem;
     }
 }
