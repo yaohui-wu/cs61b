37ca798646537841546efe86e1e91a8a733442be
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-14 18:44:46 -0500

    Update Project 1: Data Structures

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,51 +1,62 @@
 package deque;
 
-/** Implementation of a deque (double-ended queue) using a doubly linked list
- *  with a sentinel node.
+/** Implementation of a deque (double-ended queue) using a circular doubly
+ *  linked list with a sentinel node.
  *  @author Yaohui Wu
  */
 public class LinkedListDeque<T> {
 
-    public static class Node<T> {
+    public class Node {
         private T item;
-        private Node<T> next;
-        private Node<T> previous;
+        private Node next;
+        private Node previous;
 
         public Node(T item) {
             this.item = item;
-            this.next = null;
-            this.previous = null;
+            next = null;
+            previous = null;
         }
-
-        public T getitem() { return item; }
     }
 
 
-    private Node<T> sentinel;
+    private Node sentinel;
     private int size;
 
     public LinkedListDeque() {
-        sentinel = null;
+        sentinel = new Node(null);
+        sentinel.next = sentinel;
+        sentinel.previous = sentinel;
         size = 0;
     }
 
     public void addFirst(T item) {
-        Node<T> first = new Node<>(item);
-        first.next = sentinel;
-        sentinel = first;
+        Node first = new Node(item);
+        Node current = sentinel.next;
+        current.previous = first;
+        first.next = current;
+        first.previous = sentinel;
+        sentinel.next = first;
         size++;
     }
 
-    public void addLast(T item) {}
+    public void addLast(T item) {
+        Node last = new Node(item);
+        Node current = sentinel.previous;
+        current.next = last;
+        last.previous = current;
+        last.next = sentinel;
+        sentinel.previous = last;
+        size++;
+    }
 
     public boolean isEmpty() { return size == 0; }
 
     public int size() { return size; }
     
     public void printDeque() {
+        Node current = sentinel.next;
         for (int i = 0; i < size; i++) {
-            Node<T> current = sentinel;
-            System.out.print(current.item);
+            System.out.print(current.item + " ");
             current = current.next;
         }
         System.out.println();
@@ -53,12 +64,13 @@ public class LinkedListDeque<T> {
 
     public T removeFirst() {
         if (size <= 0) { return null; }
-        T firstItem = sentinel.item;
-        sentinel = sentinel.next;
-        return firstItem;
+        size--;
+        return null;
     }
 
     public T removeLast() {
         if (size <= 0) { return null; }
+        size--;
+        return null;
     }
 }
