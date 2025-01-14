161b1f01ee9c79ecf73baee02e3359e9dac35fc4
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-14 16:41:06 -0500

    Update Project 1: Data Structures

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -6,33 +6,75 @@ package deque;
 public class LinkedListDeque<T> {
 
     public static class Node<T> {
-        private T data;
+        private T item;
         private Node<T> next;
 
-        public Node(T data) {
-            this.data = data;
+        public Node(T item) {
+            this.item = item;
             this.next = null;
         }
 
-        public Node(T data, Node<T> next) {
-            this.data = data;
+        public Node(T item, Node<T> next) {
+            this.item = item;
             this.next = next;
         }
 
-        public T getData() { return data; }
+        public T getitem() { return item; }
     }
 
 
     private Node<T> head;
+    private Node<T> tail;
     private int size;
 
     public LinkedListDeque() {
         head = null;
+        tail = null;
         size = 0;
     }
 
-    public LinkedListDeque(T data) {
-        head = new Node<>(data);
+    public LinkedListDeque(T item) {
+        head = new Node<>(item);
+        tail = head;
         size = 1;
+    }
+
+    public void addFirst(T item) {
+        Node<T> first = new Node<>(item, head);
+        head = first;
+        size++;
+    }
+
+    public void addLast(T item) {
+        Node<T> last = new Node<>(item);
+        tail.next = last;
+        tail = last;
+        size++;
+    }
+
+    public boolean isEmpty() { return size == 0; }
+
+    public int size() { return size; }
+    
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            Node<T> current = head;
+            System.out.print(current.item);
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size <= 0) { return null; }
+        T firstItem = head.item;
+        head = head.next;
+        return firstItem;
+    }
+
+    public T removeLast() {
+        if (size <= 0) { return null; }
+        T lastItem = tail.item;
+        return lastItem;
     }
 }
