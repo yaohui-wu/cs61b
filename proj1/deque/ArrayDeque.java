6c55c1e1d1a255cf25a2fba0fb0bdd19f4676595
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-15 17:32:11 -0500

    Update Project 1: Data Structures

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,27 +1,47 @@
 package deque;
 
-/** Implementation of a deque (double-ended queue) using a resizable array.
+import java.lang.Math;
+
+/** Implementation of a deque (double-ended queue) using a resizable circular
+ *  array.
  *  @author Yaohui Wu
  */
 public class ArrayDeque<T> {
     private int capacity; // Size of the array.
     private T[] items;
     private int size; // Size of the deque.
+    private int nextFirst; // Position of the next first item in the deque.
+    private int nextLast; // Position of the next last item in the deque.
 
     /** Creates an empty array deque with an initial capacity of 8. */
     public ArrayDeque() {
         capacity = 8;
-        items = new T[capacity];
+        items = (T[]) new Object[capacity];
         size = 0;
+        // Initializes the deque in the middle of the array.
+        nextFirst = capacity / 2;
+        nextLast = nextFirst + 1;
+    }
+
+    public int getFirstIndex() {
+        int first = nextFirst + 1;
+        if (first == size) { first %= size; }
+        return first;
     }
 
     /** Adds an item of type T to the front of the deque in constant time. */
-    public void addFirst(T item) {}
+    public void addFirst(T item) {
+        items[nextFirst] = item;
+        nextFirst -= 1;
+        if (nextFirst < 0) { nextFirst = Math.floorMod(nextFirst, size); }
+        size += 1;
+    }
 
     /** Adds an item of type T to the back of the deque in constant time. */
     public void addLast(T item) {
-        // Last item in the deque is always in position size - 1.
-        items[size] = item;
+        items[nextLast] = item;
+        nextLast += 1;
+        if (nextLast == size) { nextLast %= size; }
         size += 1;
     }
 
@@ -35,34 +55,51 @@ public class ArrayDeque<T> {
      *  space, then prints out a new line.
      */
     public void printDeque() {
-        T item;
+        // Position of the first element of the deque.
+        int index = nextFirst + 1;
         for (int i = 0; i < size - 1; i += 1) {
-            item = items[i];
-            System.out.print(item + " ");
+            if (index == size) { index %= size; }
+            System.out.print(items[index] + " ");
+            index += 1;
         }
-        System.out.println(items[size - 1]);
+        System.out.println(items[index]);
     }
 
     /** Removes and returns the item at the front of the deque. If no such
      *  item exists, returns null.
      */
-    public T removeFirst() {}
+    public T removeFirst() {
+        int first = getFirstIndex();
+        T item = items[first];
+        items[first] = null;
+        nextFirst = first;
+        size -= 1;
+        return item;
+    }
 
     /** Removes and returns the item at the back of the deque. If no such item
      *  exists, returns null.
      */
     public T removeLast() {
-        T last = items[size - 1];
+        int last = nextLast - 1;
+        if (last < 0) { last = Math.floorMod(last, size); }
+        T item = items[last];
+        items[last] = null;
+        nextLast = last;
         size -= 1;
-        return last;
+        return item;
     }
 
-    /**
-     * Gets the item at the given index using iteration, where 0 is the front,
-     * 1 is the next item, and so forth. If no such item exists, returns null.
+    /** Gets the item at the given index in constant time, where 0 is the
+     *  front, 1 is the next item, and so forth. If no such item exists,
+     *  returns null.
      */
     public T get(int index) {
-        if (index >= size) { return null; }
-        return items[index];
+        if (index >= size) {return null; }
+        int first = getFirstIndex();
+        index += first;
+        if (index >= size) { index %= size; }
+        T item = items[index];
+        return item;
     }
 }
