aba8e329378952e575a6e1fe01b98c980065c6ef
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-21 15:21:30 -0500

    Update Lab 7: BSTMap

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,54 @@
+package bstmap;
+
+/** Implementation of a map using a binary search tree (BST).
+ *  @author Yaohui Wu
+ */
+public class BSTMap implements Map61B<K, V> {
+    
+    /** Removes all of the mappings from this map. */
+    void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Returns true if this map contains a mapping for the specified key. */
+    boolean containsKey(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    V get(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    void put(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Returns a Set view of the keys contained in this map. Not required for Lab 7.
+     * If you don't implement this, throw an UnsupportedOperationException. */
+    Set<K> keySet(); {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 7. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 7. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+}
