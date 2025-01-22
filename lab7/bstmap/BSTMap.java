97f939b450606d18288c10da2c4b16fe967db5b9
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-01-21 19:29:54 -0500

    Update Lab 7: BSTMap

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,16 +1,20 @@
 package bstmap;
 
+import java.util.Set;
+import java.util.Iterator;
+
 /** Map based on a binary search tree (BST).
  *  @author Yaohui Wu
  */
-public class BSTMap implements Map61B<K, V> {
+public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
     private Node root; // Root of the BST.
 
     /** BST node with a key value pair. */
     private class Node {
         private K key; // Sorts by key.
         private V value; // Associated data.
-        private Node left, right; // Left and right subtrees.
+        private BSTMap left; // Left subtree.
+        private BSTMap right; // Right subtree.
         private int size; // Numbers of nodes in the tree.
 
         public Node(K newKey, V newValue, int newSize) {
@@ -22,9 +26,9 @@ public class BSTMap implements Map61B<K, V> {
         }
     }
 
-    /** Initializes an empty map. */
+    /** Initializes an empty BST map. */
     public BSTMap() {
-        root = null;
+        root = new Node(null, null, 0);
     }
     
     /** Removes all of the mappings from this map. */
@@ -34,66 +38,71 @@ public class BSTMap implements Map61B<K, V> {
 
     /** Returns true if this map contains a mapping for the specified key. */
     public boolean containsKey(K key) {
-        if (root == null) {
+        if (root == null || root.key == null) {
             return false;
         }
-        if (key == root.key) {
+        int relation = key.compareTo(root.key);
+        if (relation == 0) {
             return true;
-        } else if (key < root.key) {
-            Node left = root.left;
+        } else if (relation < 0) {
+            BSTMap left = root.left;
             return left.containsKey(key);
         } else {
-            Node right = root.right;
+            BSTMap right = root.right;
             return right.containsKey(key);
         }
-        return false;
     }
 
     /** Returns the value to which the specified key is mapped, or null if
      *  this map contains no mapping for the key.
      */
     public V get(K key) {
-        if (!containsKey()) {
+        if (!containsKey(key)) {
             return null;
         }
-        if (key == root.key) {
+        int relation = key.compareTo(root.key);
+        if (relation == 0) {
             return root.value;
-        } else if (key < root.key) {
-            Node left = root.left;
-            left.get(key);
+        } else if (relation < 0) {
+            BSTMap left = root.left;
+            return (V) left.get(key);
         } else {
-            Node right = root.right;
-            right.get(key);
+            BSTMap right = root.right;
+            return (V) right.get(key);
         }
     }
 
     /** Returns the number of key-value mappings in this map. */
-    int size() {
-        int size;
+    public int size() {
         if (root == null) {
-            size = 0;
-        } else {
-            size = 1;
-            Node left = root.left;
+            int size = 0;
+            return size;
+        }
+        int size = 1;
+        BSTMap left = root.left;
+        if (left != null) {
             size += left.size();
-            Node right = root.right;
+        }
+        BSTMap right = root.right;
+        if (right != null) {
             size += right.size();
         }
         return size;
     }
 
     /** Associates the specified value with the specified key in this map. */
-    void put(K key, V value) {
-        if (containsKey(key)) {
+    public void put(K key, V value) {
+        if (containsKey(key) || root.key == null || key == null) {
             return;
         }
-        if (key == root.key) {
+        int relation = key.compareTo(root.key);
+        if (relation == 0) {
             root.value = value;
-        } else if (key < root.key) {
-            Node left = root.left;
+        } else if (relation < 0) {
+            BSTMap left = root.left;
             left.put(key, value);
         } else {
-            Node right = root.right;
+            BSTMap right = root.right;
             right.put(key, value);
         }
     }
@@ -102,7 +111,7 @@ public class BSTMap implements Map61B<K, V> {
      *  Lab 7.
      *  If you don't implement this, throw an UnsupportedOperationException.
      */
-    public Set<K> keySet(); {
+    public Set<K> keySet() {
         throw new UnsupportedOperationException();
     }
 
@@ -119,6 +128,10 @@ public class BSTMap implements Map61B<K, V> {
      *  throw an UnsupportedOperationException.
      */
     public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
     }
 
